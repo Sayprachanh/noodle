7dc9f1dc3ac83b014e2083b7e56bea3a54b6969e
Sayprachanh <cocoonsayprachanh@gmail.com>
2026-02-07 10:30:50 +0300

    testing cmd/ideas

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
-use std::io::{self, Write};
+use std::io::{self, Write, Read};
 use std::fs;
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
 
 
 
@@ -24,7 +27,7 @@ pub fn run_prog(){
         if check == "noodle"{
             let _ = workspace_mode();
         }else if check == "clean"{
-            cleaning_mode();
+            let _ = cleaning_mode();
         }else{
             println!("unknown mode");
         
@@ -37,14 +40,15 @@ fn workspace_mode() -> io::Result<()>{
     println!("Entering Workspace (Noodle) Mode | type (exit) to leave");
     //io::stdout().flush().unwrap();
 
-    let file_path = ".noodle_history";
-  
-    //if file !exist = create else open the file
-    let mut file = fs::OpenOptions::new()
-            .create(true).append(true).open(file_path)?;
-
-    writeln!(&mut file, "{}", "start section")?;
+    let home = env::var("HOME").expect("cannot read file home dir");
+    let mut path = PathBuf::from(home);
+        path.push(".zsh_history");
     
+    let mut file = fs::OpenOptions::new().append(true).open(&path)?;
+
+    writeln!(file, "{}", "start section")?;
+    
+    // will work on this later
     loop {
         let mut content = String::new();
         io::stdin().read_line(&mut content).expect("fail to read line (content)");
@@ -52,7 +56,7 @@ fn workspace_mode() -> io::Result<()>{
         file.write_all(content.as_bytes())?;
 
         //exit
-        if content.trim() == "exit"{
+        if content.trim() == "exit noodle mode"{
             writeln!(&mut file, "{}", "end Section")?;
             println!("complete work");
             break;
@@ -64,13 +68,37 @@ fn workspace_mode() -> io::Result<()>{
     
 }
 
-fn cleaning_mode(){
+fn cleaning_mode() -> io::Result<()>{
     println!("Entering Cleaning Mode");
+    
+
+    let home = env::var("HOME").expect("cannot read file home dir");
+    let mut p = PathBuf::from(home);
+        p.push(".zsh_history");
+    
+
+    //let mut file = fs::File::open(&p)?;
+    //let mut buff = Vec::new();
+
+       //file.read_to_end(&mut buff)?;
+    let mut file = fs::OpenOptions::new().append(true).open(&p)?;
+
+    //let c = String::from_utf8_lossy(&buff);
+    writeln!(file, "{}", "start section")?;
+
+    Ok(())
 
 }
 
 fn main() {
-   run_prog();
+   //run_prog();
+
+    // test func
+    let a = Command::new("echo").arg("test").output().expect("dail");
+    if a.status.success(){
+        let out = str::from_utf8(&a.stdout).expect("fail");
+        println!("output: {}", out);
+    }
 }
 
 
